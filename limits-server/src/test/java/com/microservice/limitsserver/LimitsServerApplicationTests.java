0fe36bf9d20ad300b2e04902c576910c6a4f4b56
allhpy35 <allhpy35@gmail.com>
2021-12-22 14:56:57 +0900

    Initial commit

---
+++ limits-server/src/test/java/com/microservice/limitsserver/LimitsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.limitsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
